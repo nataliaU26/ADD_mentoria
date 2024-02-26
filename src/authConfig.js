62ab9daf81ad06ce5a0644ac6b6dbca1a897c4c0
nataliaU26 <nataliacamilaumana@gmail.com>
2024-02-25 20:24:32 -0500

    first commit

---
+++ src/authConfig.js
@@ -0,0 +1,8 @@
+// src/authConfig.js
+export const msalConfig = {
+    auth: {
+        clientId: "Tu Application (client) ID aquí",
+        authority: "https://login.microsoftonline.com/Tu Directory (tenant) ID aquí",
+        redirectUri: "http://localhost:3000"
+    }
+};
