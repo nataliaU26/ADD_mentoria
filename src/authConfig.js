864843143c7da1b27f692eec12534dc9a307bad7
nataliaU26 <nataliacamilaumana@gmail.com>
2024-02-27 17:54:39 -0500

    eliminate worklow

--- src/authConfig.js
+++ src/authConfig.js
@@ -2,7 +2,7 @@
 export const msalConfig = {
     auth: {
         clientId: "7d6bbc08-42af-481b-9bce-b4a521cdfd45",
-        authority: "https://login.microsoftonline.com/{21ae5a36-8b25-4a5e-95b3-a1181c56061c}/v2.0",
+        authority: "https://login.microsoftonline.com/21ae5a36-8b25-4a5e-95b3-a1181c56061c/oauth2/v2.0/authorize",
         redirectUri: "https://proud-bush-0271aa20f.4.azurestaticapps.net"
     }
 };
@@ -10,3 +10,5 @@ export const msalConfig = {
 export const loginRequest = {
     scopes: ["User.Read"]
  };
+
+ //{}
